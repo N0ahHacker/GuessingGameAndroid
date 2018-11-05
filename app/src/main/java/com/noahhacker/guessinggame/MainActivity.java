70db1cfc6dc34f9e7b329245ddaadddaa7b2e2a9
Noah Hacker <noah.hacker8@gmail.com>
2018-11-04 20:00:07 -0400

    Completed the menu items including settings and statistics

--- app/src/main/java/com/noahhacker/guessinggame/MainActivity.java
+++ app/src/main/java/com/noahhacker/guessinggame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.noahhacker.guessinggame;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnGuess;
     private TextView lblOutput;
     private int theNumber;
+    private int range =100;
+    private TextView lblRange;
     private int numberOfTries;
 
     public void checkGuess() {
@@ -38,10 +42,16 @@ public class MainActivity extends AppCompatActivity {
                 message = guess +" is correct. You got it in "+ numberOfTries+" guesses! Let's play again!";
                 Toast.makeText(MainActivity.this, message,
                         Toast.LENGTH_SHORT).show();
+                ///adds one to number of games won for each win
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                int gamesWon = preferences.getInt("gamesWon", 0) +1;
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("gamesWon", gamesWon);
+                editor.apply();
                 newGame();
             }
         }catch (Exception e) {
-            message = "Enter a whole number between 1 and 100";
+            message = "Enter a whole number between 1 and "+ range+ ".";
         }finally {
             lblOutput.setText(message);
             txtGuess.requestFocus();
@@ -51,7 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void newGame() {
-        theNumber = (int) (Math.random() * 100 + 1);
+        theNumber = (int) (Math.random() * range + 1);
+        lblRange.setText("Enter a number between 1 and "+ range + ".");
+        txtGuess.setText(""+ range/2);
+        txtGuess.requestFocus();
+        txtGuess.selectAll();
         numberOfTries = 0;
     }
 
@@ -62,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         txtGuess = (EditText)findViewById(R.id.txtGuess);
         btnGuess = (Button) findViewById(R.id.btnGuess);
         lblOutput = (TextView) findViewById(R.id.lblOutput);
+        lblRange = (TextView) findViewById(R.id.textView3);
         newGame();
 
         btnGuess.setOnClickListener(new View.OnClickListener() {
@@ -94,11 +109,49 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings:
+                final CharSequence[] items = {"1 to 100", "1 to 1000", "1 to 10000"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Select the Range: ");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                   ///@Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch(item) {
+                            case 0:
+                                range =100;
+                                newGame();
+                                break;
+                            case 1:
+                                range = 1000;
+                                newGame();
+                                break;
+                            case 2:
+                                range = 10000;
+                                newGame();
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
                 return true;
             case R.id.action_newGame:
                 newGame();
                 return true;
             case R.id.action_gamestats:
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                int gamesWon = preferences.getInt("gamesWon", 0);
+                AlertDialog statDialog = new AlertDialog.Builder(MainActivity.this).create();
+                statDialog.setTitle("Guessing Game Stats");
+                statDialog.setMessage("You have won " +gamesWon+ " games. Good Job!");
+                statDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                statDialog.show();
                 return true;
             case R.id.action_about:
                 AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
